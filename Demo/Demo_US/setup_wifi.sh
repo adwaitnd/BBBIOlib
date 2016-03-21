574241fb0a4e9b02ad8797a4e329f557167460b9
skcmars <root@beaglebone.localdomain>
2016-03-21 15:47:05 -0400

    add tests

--- Demo/Demo_US/setup_wifi.sh
+++ Demo/Demo_US/setup_wifi.sh
@@ -21,12 +21,14 @@ if [ $? -eq 1 ]
 then
 	ifup wlan0
 	sleep 15	
+	route add default gw 128.237.224.1 
 fi
 
 RES=$(ping 8.8.8.8 -c 1 | egrep 'Unreachable | unreachable' | wc -l)
 
 while [ $RES -ne 0 ]
 do
+	echo "Ping failed, reseting the interface ..."
 	ifdown wlan0
 	sleep 3
 	ifup wlan0
